48ee26af1fe4edb912b52357f0cad269b5e2bbd1
Андрей Замятин <andreizamyat1n@yandex.ru>
2024-04-02 15:01:30 +0500

    Добавление первой версии самостоятельного сервера

---
+++ main_client/main_client.cpp
@@ -0,0 +1,19 @@
+﻿// Клиент
+#include <boost/asio.hpp>
+#include <iostream>
+
+int main() 
+{
+    boost::asio::io_service io_service;
+    boost::asio::ip::tcp::socket socket(io_service);
+    socket.connect(boost::asio::ip::tcp::endpoint(boost::asio::ip::address::from_string("127.0.0.1"), 12345));
+
+    std::string file_name;
+    std::cout << "Enter file name: ";
+    std::getline(std::cin, file_name);
+    boost::asio::write(socket, boost::asio::buffer(file_name + "\n", file_name.size() + 1));
+
+    char buffer[1024];
+    size_t bytes_transferred = boost::asio::read(socket, boost::asio::buffer(buffer), boost::asio::transfer_all());
+    std::cout << buffer << std::endl;
+}
---
+++ main_server/main_server.cpp
@@ -0,0 +1,31 @@
+﻿// Сервер
+#include <boost/asio.hpp>
+#include <boost/thread.hpp>
+#include <fstream>
+#include <string>
+
+void handle_client(boost::asio::ip::tcp::socket socket) 
+{
+    std::string file_name;
+    boost::asio::read_until(socket, boost::asio::dynamic_buffer(file_name), "\n");
+
+    std::ifstream file(file_name);
+    if (!file.is_open()) {
+        boost::asio::write(socket, boost::asio::buffer("File not found\n", 17));
+    }
+    else {
+        std::string content((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+        boost::asio::write(socket, boost::asio::buffer(content + "\n", content.size() + 1));
+    }
+}
+
+int main() {
+    boost::asio::io_service io_service;
+    boost::asio::ip::tcp::acceptor acceptor(io_service, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), 12345));
+
+    while (true) {
+        boost::asio::ip::tcp::socket socket(io_service);
+        acceptor.accept(socket);
+        boost::thread(handle_client, std::move(socket)).detach();
+    }
+}
