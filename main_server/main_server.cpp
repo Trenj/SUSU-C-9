ec5ab2ee719bb1be093ac29b7bccc8aaf0292900
Андрей Замятин <andreizamyat1n@yandex.ru>
2024-04-10 13:55:26 +0500

    Фикс разрыва соединения после первого запроса

--- main_server/main_server.cpp
+++ main_server/main_server.cpp
@@ -12,40 +12,45 @@
 
 // Обработчик запроса клиента
 void client_handler(SOCKET client_socket) {
-    std::string filename;
-    char buffer[1024];
+    while (true) {
+        std::string filename;
+        char buffer[1024] = { 0 };
 
-    // Получаем имя файла от клиента
-    int bytesReceived = recv(client_socket, buffer, sizeof(buffer), 0);
-    if (bytesReceived > 0) {
-        buffer[bytesReceived] = '\0'; // Добавляем нулевой символ в конец строки
-        filename = std::string(buffer);
+        // Получаем имя файла от клиента
+        int bytesReceived = recv(client_socket, buffer, sizeof(buffer), 0);
+        if (bytesReceived > 0) {
+            buffer[bytesReceived] = '0'; // Добавляем нулевой символ в конец строки
+            filename = std::string(buffer);
 
-        // Открываем файл
-        std::ifstream file(filename);
-        if (file.is_open()) {
-            std::stringstream file_content;
-            file_content << file.rdbuf();
-            file.close();
+            // Открываем файл
+            std::ifstream file(filename);
+            if (file.is_open()) {
+                std::stringstream file_content;
+                file_content << file.rdbuf();
+                file.close();
 
-            // Отправляем содержимое файла клиенту
-            send(client_socket, file_content.str().c_str(), static_cast<int>(file_content.str().size()), 0);
+                // Отправляем содержимое файла клиенту
+                send(client_socket, file_content.str().c_str(), static_cast<int>(file_content.str().size()), 0);
+            }
+            else {
+                // Если файл не может быть открыт, отправляем ошибку
+                std::string error_msg = "Error: file not found";
+                send(client_socket, error_msg.c_str(), static_cast<int>(error_msg.size()), 0);
+            }
+        }
+        else if (bytesReceived == 0) {
+            std::cout << "Connection closing..." << std::endl;
+            break;
         }
         else {
-            // Если файл не может быть открыт, отправляем ошибку
-            std::string error_msg = "Error: file not found";
-            send(client_socket, error_msg.c_str(), static_cast<int>(error_msg.size()), 0);
+            std::cerr << "recv failed: " << WSAGetLastError() << std::endl;
+            break;
         }
-    }
-    else if (bytesReceived == 0) {
-        std::cout << "Connection closing..." << std::endl;
-    }
-    else {
-        std::cerr << "recv failed: " << WSAGetLastError() << std::endl;
     }
 
     closesocket(client_socket);
 }
+
 
 int main() {
     WSADATA wsaData;
